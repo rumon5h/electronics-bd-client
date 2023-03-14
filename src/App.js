99721b4931fb70181221b27ac521a14a2869ced6
rumon5h <mdrumonkhan48519@gmail.com>
2023-03-14 20:42:44 +0600

    files added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
+import Footer from './pages/Home/Footer';
 import Home from './pages/Home/Home';
+import Navbar from './pages/Navbar';
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+     <Navbar/>
      <Routes>
       <Route path='/' element={Home}></Route>
+
      </Routes>
+     <Footer/>
     </div>
   );
 }
