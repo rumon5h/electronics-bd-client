8ff7f6eea5ca5dc5ea90cc932208931e3bf44654
rumon5h <mdrumonkhan48519@gmail.com>
2023-03-16 20:56:06 +0600

    log in and signup

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 import Footer from './pages/Home/Footer';
 import Home from './pages/Home/Home';
 import Navbar from './pages/Navbar';
+import LogIn from './pages/Signin/LogIn';
+import SignUp from './pages/Signin/SignUp';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
      <Routes>
-      <Route path='/' element={Home}></Route>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/login' element={<LogIn/>}></Route>
+      <Route path='/signup' element={<SignUp/>}></Route>
 
      </Routes>
+     
      <Footer/>
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div style={{marginTop: '1000px'}}>
             <h1>Welcome to home page.</h1>
         </div>
     );
